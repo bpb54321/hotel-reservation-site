dbbb09042297a0afb17cd3effc68e06abe7be55a
Brian Blosser <bpb54321@gmail.com>
2019-09-18 16:02:21 -0400

    Added a bunch of project files and created the main page template files

---
+++ src/pages/Rooms.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const RoomsPage = () => {
+  return (
+    <h1>Rooms Page</h1>
+  );
+};
---
+++ src/pages/SingleRoomPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const SingleRoomPage = () => {
+  return (
+    <h1>Single Room Page</h1>
+  );
+};
