01306902491ffa8cec1067d4bdecd6427b9474cf
Brian Blosser <bpb54321@gmail.com>
2019-09-23 16:57:16 -0400

    Checking that the list times switch from horizontal to vertical alignment.
    When the window width changes.

--- cypress/integration/NavLinkList.spec.js
+++ cypress/integration/NavLinkList.spec.js
@@ -1,5 +1,51 @@
 import {BREAKPOINTS} from "../../src/context/WindowWidthZoneContext";
 
+/**
+ *
+ * @param $elements {JQuery} A jQuery collection of elements.
+ */
+function assertThatElementsAreHorizontallyAligned($elements) {
+  // Initialize with a dummy value to get intelligent type completion
+  let previousItemBoundingRect = new DOMRect(0, 0, 0, 0);
+
+  for (let i = 0; i < $elements.length; i++) {
+    const listItem = $elements[i];
+    const currentItemBoundingRect = listItem.getBoundingClientRect();
+    if (i > 0) {
+      expect(currentItemBoundingRect.left - previousItemBoundingRect.right).to.be.closeTo(0, 1);
+      expect(currentItemBoundingRect.top - previousItemBoundingRect.top).to.be.closeTo(0, 1);
+      expect(currentItemBoundingRect.bottom - previousItemBoundingRect.bottom).to.be.closeTo(0, 1);
+    }
+
+    previousItemBoundingRect = currentItemBoundingRect;
+  }
+}
+
+/**
+ *
+ * @param $elements {JQuery} A jQuery collection of elements.
+ */
+function assertThatElementsAreVerticallyAligned($elements) {
+  // Initialize with a dummy value to get intelligent type completion
+  let previousItemBoundingRect = new DOMRect(0, 0, 0, 0);
+
+  for (let i = 0; i < $elements.length; i++) {
+    const listItem = $elements[i];
+    const currentItemBoundingRect = listItem.getBoundingClientRect();
+    if (i > 0) {
+      // Later list items will be visually lower on the screen
+      expect(currentItemBoundingRect.top - previousItemBoundingRect.bottom)
+        .to.be.closeTo(0, 1, `Expected $listItems[${i}].top to be at the same spot as ` +
+        `$listItems[${i - 1}].bottom`);
+      expect(currentItemBoundingRect.left - previousItemBoundingRect.left)
+        .to.be.closeTo(0, 1, `Expected $listItems[${i}].left to be aligned with ` +
+        `$listItems[${i - 1}].left`);
+    }
+
+    previousItemBoundingRect = currentItemBoundingRect;
+  }
+}
+
 context('NavLinkList', () => {
   beforeEach(() => {
     cy.visit("/")
@@ -9,23 +55,8 @@ context('NavLinkList', () => {
     cy.viewport(BREAKPOINTS.DESKTOP, 600);
 
     cy.get(`[data-testid="nav-link-list-item`)
-      .then(($listItems) => {
-
-        // Initialize with a dummy value to get intelligent type completion
-        let previousItemBoundingRect = new DOMRect(0, 0, 0, 0);
-
-        for (let i = 0; i < $listItems.length; i++) {
-          const listItem = $listItems[i];
-          const currentItemBoundingRect = listItem.getBoundingClientRect();
-          if (i > 0) {
-            expect(currentItemBoundingRect.left - previousItemBoundingRect.right).to.be.closeTo(0, 1);
-            expect(currentItemBoundingRect.top - previousItemBoundingRect.top).to.be.closeTo(0, 1);
-            expect(currentItemBoundingRect.bottom - previousItemBoundingRect.bottom).to.be.closeTo(0, 1);
-          }
-
-          previousItemBoundingRect = currentItemBoundingRect;
-        }
-
+      .should(($listItems) => {
+        assertThatElementsAreHorizontallyAligned($listItems);
       });
 
   });
@@ -35,26 +66,24 @@ context('NavLinkList', () => {
 
     cy.get(`[data-testid="nav-link-list-item`)
       .should(($listItems) => {
+        assertThatElementsAreVerticallyAligned($listItems);
+      });
+  });
 
-        // Initialize with a dummy value to get intelligent type completion
-        let previousItemBoundingRect = new DOMRect(0, 0, 0, 0);
+  it(`changes the list item layout from horizontal to vertical when transitioning from desktop to mobile`, () => {
+    cy.viewport(BREAKPOINTS.DESKTOP + 10, 600);
 
-        for (let i = 0; i < $listItems.length; i++) {
-          const listItem = $listItems[i];
-          const currentItemBoundingRect = listItem.getBoundingClientRect();
-          if (i > 0) {
-            // Later list items will be visually lower on the screen
-            expect(currentItemBoundingRect.top - previousItemBoundingRect.bottom)
-              .to.be.closeTo(0, 1, `Expected $listItems[${i}].top to be at the same spot as ` +
-              `$listItems[${i - 1}].bottom`);
-            expect(currentItemBoundingRect.left - previousItemBoundingRect.left)
-              .to.be.closeTo(0, 1, `Expected $listItems[${i}].left to be aligned with ` +
-              `$listItems[${i - 1}].left`);
-          }
+    cy.get(`[data-testid="nav-link-list-item`)
+      .as(`nav-link-list-items`)
+      .should(($listItems) => {
+        assertThatElementsAreHorizontallyAligned($listItems);
+      });
 
-          previousItemBoundingRect = currentItemBoundingRect;
-        }
+    cy.viewport(BREAKPOINTS.DESKTOP - 10, 600);
 
+    cy.get(`@nav-link-list-items`)
+      .should(($listItems) => {
+        assertThatElementsAreVerticallyAligned($listItems);
       });
   });
 
